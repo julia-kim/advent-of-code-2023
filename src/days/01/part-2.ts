30cfdea44dc43c985742541f1dd358a3a20d8be2
Julia Kim <juliamk@pm.me>
2023-12-01 20:01:13 -0500

    day 1 part 2 refactor

--- src/days/01/part-2.ts
+++ src/days/01/part-2.ts
@@ -1,68 +1,66 @@
 import { readInputFromFile } from "../../utils/io";
 
 export function part2(input?: string[]) {
-    const numberStrings = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    const numberMap = new Map();
-    numberMap.set("1", "1");
-    numberMap.set("2", "2");
-    numberMap.set("3", "3");
-    numberMap.set("4", "4");
-    numberMap.set("5", "5");
-    numberMap.set("6", "6");
-    numberMap.set("7", "7");
-    numberMap.set("8", "8");
-    numberMap.set("9", "9");
-    numberMap.set("one", "1");
-    numberMap.set("two", "2");
-    numberMap.set("three", "3");
-    numberMap.set("four", "4");
-    numberMap.set("five", "5");
-    numberMap.set("six", "6");
-    numberMap.set("seven", "7");
-    numberMap.set("eight", "8");
-    numberMap.set("nine", "9");
+    const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    const map =
+        new Map()
+            .set("1", "1")
+            .set("2", "2")
+            .set("3", "3")
+            .set("4", "4")
+            .set("5", "5")
+            .set("6", "6")
+            .set("7", "7")
+            .set("8", "8")
+            .set("9", "9")
+            .set("one", "1")
+            .set("two", "2")
+            .set("three", "3")
+            .set("four", "4")
+            .set("five", "5")
+            .set("six", "6")
+            .set("seven", "7")
+            .set("eight", "8")
+            .set("nine", "9");
     const lines = input ?? readInputFromFile(__dirname);
 
     const calibrationValues = lines.map((line) => {
-        var lowest = Number.MAX_SAFE_INTEGER;
-        var highest = -1;
-        var firstString = "";
-        var lastString = "";
-        console.log(line)
-        for (var i = 0; i < numberStrings.length; i++) {
+        var firstDigitIndex = Number.MAX_SAFE_INTEGER;
+        var lastDigitIndex = -1;
+        var firstDigit = "";
+        var lastDigit = "";
+        for (var i = 0; i < numbers.length; i++) {
             const startingIndices = [];
-            let indexOccurence = line.indexOf(numberStrings[i], 0);
-            while (indexOccurence >= 0) {
-                startingIndices.push(indexOccurence);
-                indexOccurence = line.indexOf(numberStrings[i], indexOccurence + 1);
+            let indexOccurrence = line.indexOf(numbers[i], 0);
+            while (indexOccurrence >= 0) {
+                startingIndices.push(indexOccurrence);
+                indexOccurrence = line.indexOf(numbers[i], indexOccurrence + 1);
             }
-            const firstFound = Math.min(...startingIndices)
-            if (firstFound < lowest) {
-                lowest = firstFound;
-                firstString = numberStrings[i];
+            var startingIndex = Math.min(...startingIndices)
+            if (startingIndex < firstDigitIndex) {
+                firstDigitIndex = startingIndex;
+                firstDigit = numbers[i];
             }
 
             for (var j = 0; j < startingIndices.length; j++) {
-                const lastFound = startingIndices[j]
-                const lastI = lastFound + numberStrings[i].length - 1
-                if (numberMap.get(lastString) < numberMap.get(numberStrings[i]) && lastFound > highest) {
-                    highest = lastFound;
-                    lastString = numberStrings[i];
+                startingIndex = startingIndices[j]
+                if (map.get(lastDigit) < map.get(numbers[i]) && startingIndex > lastDigitIndex) {
+                    lastDigitIndex = startingIndex;
+                    lastDigit = numbers[i];
                 }
-                if (highest > 0) {
-                    const lastIndex = highest + lastString.length - 1
-                    if (lastIndex < line.length && lastIndex >= lastFound) {
+                if (lastDigitIndex > 0) {
+                    const endingIndex = lastDigitIndex + lastDigit.length - 1
+                    if (endingIndex < line.length && endingIndex >= startingIndex) {
                         continue;
                     }
                 }
-                if (lastFound > highest) {
-                    highest = lastFound;
-                    lastString = numberStrings[i];
+                if (startingIndex > lastDigitIndex) {
+                    lastDigitIndex = startingIndex;
+                    lastDigit = numbers[i];
                 }
             }
         }
-        return +(numberMap.get(firstString) + numberMap.get(lastString));
+        return +(map.get(firstDigit) + map.get(lastDigit));
     });
-    console.log(calibrationValues)
     return calibrationValues.reduce((partialSum, x) => partialSum + x, 0);
 }
