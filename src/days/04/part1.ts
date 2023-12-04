af0ca3f8bd073849430952e12448b01b77055a22
Julia Kim <juliamk@pm.me>
2023-12-04 01:20:21 -0500

    day 4 part 1 wip

--- src/days/04/part1.ts
+++ src/days/04/part1.ts
@@ -1,6 +1,24 @@
+import { sum } from '../../utils/array';
 import { readInputFromFile } from '../../utils/io';
 
 export function part1(input?: string[]) {
   const lines = input ?? readInputFromFile(__dirname);
-  return lines[0];
+  const scratchcards = lines.map((line) => {
+    let matches = 0;
+    const [winningNumbers, playableNumbers] = line
+      .trim()
+      .replace(/Card \d+: /, '')
+      .split(' | ')
+      .map((numbers) => {
+        return numbers.split(/\s+/).map((x) => +x);
+      });
+    playableNumbers.forEach((number) => {
+      if (winningNumbers.includes(number)) {
+        matches++;
+      }
+    });
+    if (matches > 0) return Math.pow(2, matches - 1);
+    else return 0;
+  });
+  return sum(scratchcards);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import chalk from 'chalk';
 import { day01 } from './days/01/day01';
 import { day03 } from './days/03/day03';
+import { day04 } from './days/04/day04';
 
 const BORDER_TOP = '╔═══════════════════════════════════════════════════════════╗';
 const BORDER_BTM = '╚═══════════════════════════════════════════════════════════╝';
@@ -14,7 +15,7 @@ console.log(BORDER_BTM);
 console.log(BORDER_TOP);
 console.log('');
 
-day03();
+day04();
 
 console.log('');
 console.log(BORDER_BTM);
