97fadc0f687b1ff1f3dd558980c7ee76ee3ff6dc
Julia Kim <juliamk@pm.me>
2023-12-04 00:15:14 -0500

    init day 4

---
+++ src/tests/day04.test.ts
@@ -0,0 +1,18 @@
+import { part1 } from './part1';
+import { part2 } from './part2';
+
+describe('day XX - part 1', () => {
+  it('does the thing', () => {
+    const input = ['1', '2', '3'];
+    const result = part1(input);
+    expect(result).toBe('1');
+  });
+});
+
+describe('day XX - part 2', () => {
+  it('does the other thing', () => {
+    const input = ['1', '2', '3'];
+    const result = part2(input);
+    expect(result).toBe('1');
+  });
+});
