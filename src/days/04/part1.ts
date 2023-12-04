5655d441557d80688fc4af0522e568fccd9bd041
Julia Kim <jkim@messerfinancial.com>
2023-12-04 14:18:58 -0500

    day 4 part 2

--- src/days/04/part1.ts
+++ src/days/04/part1.ts
@@ -3,21 +3,16 @@ import { readInputFromFile } from '../../utils/io';
 
 export function part1(input?: string[]) {
   const lines = input ?? readInputFromFile(__dirname);
-  const scratchcards = lines.map((line) => {
-    let matches = 0;
+  const scratchcards = lines.map((line, i) => {
     const [winningNumbers, playableNumbers] = line
+      .replace(/Card\s*\d+: /, '')
       .trim()
-      .replace(/Card \d+: /, '')
       .split(' | ')
       .map((numbers) => {
         return numbers.split(/\s+/).map((x) => +x);
       });
-    playableNumbers.forEach((number) => {
-      if (winningNumbers.includes(number)) {
-        matches++;
-      }
-    });
-    if (matches > 0) return Math.pow(2, matches - 1);
+    let matches = playableNumbers.filter((number) => winningNumbers.includes(number));
+    if (matches.length > 0) return Math.pow(2, matches.length - 1)
     else return 0;
   });
   return sum(scratchcards);
--- src/days/04/part2.ts
+++ src/days/04/part2.ts
@@ -1,6 +1,24 @@
 import { readInputFromFile } from '../../utils/io';
+import { sum } from '../../utils/array';
 
 export function part2(input?: string[]) {
   const lines = input ?? readInputFromFile(__dirname);
-  return lines[0];
+  var copies = new Array(lines.length).fill(undefined).map((v, i) => 1)
+  const scratchcards = lines.map((line, i) => {
+    const cardIndex = +line.match(/Card\s*(\d+):/)![1] - 1;
+    const [winningNumbers, playableNumbers] = line
+      .replace(/Card\s*\d+: /, '')
+      .trim()
+      .split(' | ')
+      .map((numbers) => {
+        return numbers.split(/\s+/).map((x) => +x);
+      });
+    let noOfMatches = playableNumbers.filter((number) => winningNumbers.includes(number)).length;
+    while (noOfMatches > 0) {
+      const newIndex = cardIndex + noOfMatches
+      copies[newIndex] = copies[cardIndex] + copies[newIndex];
+      noOfMatches--
+    }
+  })
+  return sum(copies);
 }
