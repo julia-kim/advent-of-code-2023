e90943e94bae2e630c2b002f49ecc6dc78a4da62
Julia Kim <juliamk@pm.me>
2023-12-09 14:13:22 -0500

    add types

--- src/days/08/part2.ts
+++ src/days/08/part2.ts
@@ -33,8 +33,8 @@ export function part2(input?: string[]) {
     return steps;
   });
   const lcm = (...arr: number[]) => {
-    const gcd = (x, y) => (!y ? x : gcd(y, x % y));
-    const _lcm = (x, y) => (x * y) / gcd(x, y);
+    const gcd: any = (x: number, y: number) => (!y ? x : gcd(y, x % y));
+    const _lcm = (x: number, y: number) => (x * y) / gcd(x, y);
     return [...arr].reduce((a, b) => _lcm(a, b));
   };
   return lcm(...map);
