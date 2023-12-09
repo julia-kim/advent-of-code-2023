eb1ae3427cbd96d7c412b3be2e7a3b0dc2381b3f
Julia Kim <juliamk@pm.me>
2023-12-09 12:29:33 -0500

    reverse

--- src/days/09/part2.ts
+++ src/days/09/part2.ts
@@ -7,8 +7,8 @@ export function part2(input?: string[]) {
     var sequence = history
       .trim()
       .split(' ')
-      .map((n) => +n);
-    const firstValue = sequence[0];
+      .map((n) => +n).reverse();
+    const lastValue = sequence[sequence.length - 1];
     var increaseValues: number[] = [];
     let allZero = false;
     while (!allZero) {
@@ -18,17 +18,22 @@ export function part2(input?: string[]) {
         previous = n;
         return diff;
       });
-      increaseValues.push(nextSequence[0]);
+      increaseValues.push(nextSequence[nextSequence.length - 1]);
       if (nextSequence.every((num) => num === 0)) allZero = true;
       sequence = nextSequence;
     }
-    return firstValue -
-      increaseValues.reverse().map(
-        (
-          (sum) => (value) =>
-            (sum = value - sum)
-        )(0)
-      ).pop()!
+    return (
+      lastValue +
+      increaseValues
+        .reverse()
+        .map(
+          (
+            (sum) => (value) =>
+              (sum += value)
+          )(0)
+        )
+        .pop()!
+    );
   });
   return sum(extrapolatedValues);
 }
