396ecb2f23b233f42967ccebd32e6dfc8b14bc1a
Julia Kim <juliamk@pm.me>
2023-12-11 22:26:58 -0500

    day 10 wip

---
+++ src/tests/day10.test.ts
@@ -0,0 +1,18 @@
+import { part1 } from '../days/10/part1';
+import { part2 } from '../days/10/part2';
+
+describe('day 10 - part 1', () => {
+  it('does the thing', () => {
+    const input = ['1', '2', '3'];
+    const result = part1(input);
+    expect(result).toBe('1');
+  });
+});
+
+describe('day 10 - part 2', () => {
+  it('does the other thing', () => {
+    const input = ['1', '2', '3'];
+    const result = part2(input);
+    expect(result).toBe('1');
+  });
+});
